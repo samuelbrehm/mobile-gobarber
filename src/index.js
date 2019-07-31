42e4ee5007f338994d4347fef91cd984e15f9345
Samuel Brehm <samuelbrehm@gmail.com>
2019-07-31 09:52:52 -0300

    Routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 
 import './config/ReactotronConfig';
 
-// import { Container } from './styles';
-
-import Teste from '~/Teste';
+import Routes from './routes';
 
 export default function App() {
-  return <Teste />;
+  return <Routes />;
 }
